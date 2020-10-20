d4aad2000d2b1887518a8e11e89daebb67e7caab
QuangHaryanto <apple.haryanto@gmail.com>
2020-10-20 11:23:46 +0700

    first morning commit

---
+++ CustomRoundedFramework/CustomRounded.swift
@@ -0,0 +1,40 @@
+//
+//  CustomRounded.swift
+//  BeautyShape
+//
+//  Created by Haryanto Salim on 20/10/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/**
+ Access control
+ open/public
+ internal =>
+ private => accesible di dalam single file. contoh:helper function
+ */
+
+public class CustomRounded{
+    public static func allRoundedCorner(view: UIView){
+        view.layer.cornerRadius = 5.0
+        view.layer.masksToBounds = true
+    }
+    
+    public static func topLeftRightROundedCorner(view: UIView){
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner] // Top right corner, Top left corner respectively
+    }
+    
+    public static func bottomRightLeftRoundedCOrner(view:UIView){
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner] // Bottom right corner, Bottom left corner respectively
+    }
+    
+    public static func printSomething(text: String){
+        print(text)
+    }
+}
---
+++ CustomRoundedFramework/CustomRoundedFramework.h
@@ -0,0 +1,19 @@
+//
+//  CustomRoundedFramework.h
+//  CustomRoundedFramework
+//
+//  Created by Haryanto Salim on 20/10/20.
+//  Copyright © 2020 Haryanto Salim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRoundedFramework.
+FOUNDATION_EXPORT double CustomRoundedFrameworkVersionNumber;
+
+//! Project version string for CustomRoundedFramework.
+FOUNDATION_EXPORT const unsigned char CustomRoundedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRoundedFramework/PublicHeader.h>
+
+
